12f7bfdc1f0c5f006e4ec911362335e2136a8d44
Nitesh <niteshsengar9831@gmail.com>
2025-07-31 21:17:59 +0530

    debug template handler

--- Notification_Service/src/templates/templates.handler.ts
+++ Notification_Service/src/templates/templates.handler.ts
@@ -5,8 +5,12 @@ import { InternalServerError } from "../utils/errors/app.error";
 
 export async function renderMailTemplate(templateId: string, params: Record<string, any>): Promise<string> {
     const templatePath = path.join(__dirname, 'mailer', `${templateId}.hbs`)
+    // console.log(templatePath)
+
     try{
-    const content = fs.readFile(templatePath,'utf-8')
+      
+    const content = await fs.readFile(templatePath,'utf-8')
+      console.log(content) 
     const finalTemplate = Handlebars.compile(content)
     const result =finalTemplate(params)
     return result 
